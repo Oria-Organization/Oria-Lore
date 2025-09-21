de986059187b79ce21d89c8fcc67503623b0b6e7
Hunterdivin30 <divinehunter30@gmail.com>
2025-09-21 17:53:47 +0200

    Update main.js
    Correction pour faire des sous onglets.

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-function openTab(evt, tabName) {
-let tabcontent = document.getElementsByClassName("tabcontent");
+function openTab(evt, tabName, group = "1") {
+let tabcontent = document.getElementsByClassName("tabcontent" + group);
 for (let i = 0; i < tabcontent.length; i++) {tabcontent[i].style.display = "none";}
-let tablinks = document.getElementsByClassName("tablinks");
+let tablinks = document.getElementsByClassName("tablinks" + group);
 for (let i = 0; i < tablinks.length; i++) {tablinks[i].classList.remove("active");}
-document.getElementById(tabName).style.display = "block";evt.currentTarget.classList.add("active");}
-document.addEventListener("DOMContentLoaded", () => {document.querySelector(".tablinks").click();});
+document.getElementById(tabName).style.display = "block";
+evt.currentTarget.classList.add("active");}
+document.addEventListener("DOMContentLoaded", () => {
+let firstTab1 = document.querySelector(".tablinks1");
+if (firstTab1) firstTab1.click();
+let firstTab2 = document.querySelector(".tablinks2");
+if (firstTab2) firstTab2.click();});
